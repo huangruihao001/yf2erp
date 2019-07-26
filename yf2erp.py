b2f2cddeb24f86e2aa0f773b0d6107478d1f9b32
hrh <1250908116@qq.com>
2019-07-26 15:35:45 +0800

    v1.02

--- yf2erp.py
+++ yf2erp.py
@@ -2,19 +2,28 @@ import os
 import CreatXml
 import SendERP
 
-file_list = os.listdir('./documents')
-print(file_list)
-# 批量生成xml文件
-for file in file_list:
-    if '.xls' in file:
-        fileXML = CreatXml.CreatXml("./documents/" + file, "ERP表")
-        fileXML.writeXML()
-        del fileXML
+def creatXML():
+    # 批量生成xml文件
+    file_list = os.listdir('./documents')
+    print(file_list)
+    for file in file_list:
+        if '.xls' in file:
+            fileXML = CreatXml.CreatXml("./documents/" + file, "ERP表")
+            fileXML.writeXML()
+            del fileXML
 
-xml_list = os.listdir('./documents')
-print(xml_list)
-# 批量上传xml文件
-for file in xml_list:
-    if '.xml' in file:
-        print(SendERP.getXML(file))
-        
+
+def uploadXML():
+    # 批量上传xml文件
+    xml_list = os.listdir('./documents')
+    print(xml_list)
+    for file in xml_list:
+        if '.xml' in file:
+            print(SendERP.getXML(file))
+            os.remove("./documents/" + file) # 删除xml缓存文件
+
+
+if __name__ == '__main__':
+    creatXML()
+    uploadXML()
+
